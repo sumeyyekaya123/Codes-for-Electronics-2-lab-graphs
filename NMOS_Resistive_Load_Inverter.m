7b1860d91198f7278a7f975a701ff0df08cfd5b5
sumeyyekaya123 <kayawsumeyye@gmail.com>
2026-03-06 00:33:40 +0300

    Delete NMOS_Resistive_Load_Inverter.m

--- NMOS_Resistive_Load_Inverter.m
+++
@@ -1,9 +0,0 @@
-Vin=[0 1 2 3 4 5];
-Vout=[1.598 1.598 1.008 0.0487 0.0331 0.0273];
-figure;
-plot(Vin,Vout,'-o');
-grid on;
-xlabel('Vin (V)');
-ylabel('Vout (V)');
-title('Vout-Vin');
-xlim([0 5]);
